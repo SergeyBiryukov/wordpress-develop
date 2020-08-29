b3c3efa5971e376cc37d4fff11527aa31ba4354c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-29 15:18:06 +0300

    Update comment.php

--- src/wp-includes/comment.php
+++ src/wp-includes/comment.php
@@ -1046,9 +1046,9 @@ function get_comment_pages_count( $comments = null, $per_page = null, $threaded 
 
 	if ( $threaded ) {
 		$walker = new Walker_Comment;
-		$count  = (int) ceil( $walker->get_number_of_root_elements( $comments ) / $per_page );
+		$count  = ceil( $walker->get_number_of_root_elements( $comments ) / $per_page );
 	} else {
-		$count = (int) ceil( count( $comments ) / $per_page );
+		$count = ceil( count( $comments ) / $per_page );
 	}
 
 	return $count;
@@ -1189,7 +1189,7 @@ function get_page_of_comment( $comment_ID, $args = array() ) {
 
 			// Divide comments older than this one by comments per page to get this comment's page number.
 		} else {
-			$page = (int) ceil( ( $older_comment_count + 1 ) / $args['per_page'] );
+			$page = ceil( ( $older_comment_count + 1 ) / $args['per_page'] );
 		}
 	}
 
