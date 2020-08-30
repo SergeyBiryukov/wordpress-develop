0388f86d2fdf73cb07f167bbd504b114c72aaf21
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-31 02:10:05 +0300

    Update getProfile.php

--- tests/phpunit/tests/xmlrpc/wp/getProfile.php
+++ tests/phpunit/tests/xmlrpc/wp/getProfile.php
@@ -17,7 +17,7 @@ class Tests_XMLRPC_wp_getProfile extends WP_XMLRPC_UnitTestCase {
 
 		$result = $this->myxmlrpcserver->wp_getProfile( array( 1, 'subscriber', 'subscriber' ) );
 		$this->assertNotIXRError( $result );
-		$this->assertSame( $subscriber_id, $result['user_id'] );
+		$this->assertEquals( $subscriber_id, $result['user_id'] );
 		$this->assertContains( 'subscriber', $result['roles'] );
 	}
 
@@ -26,7 +26,7 @@ class Tests_XMLRPC_wp_getProfile extends WP_XMLRPC_UnitTestCase {
 
 		$result = $this->myxmlrpcserver->wp_getProfile( array( 1, 'administrator', 'administrator' ) );
 		$this->assertNotIXRError( $result );
-		$this->assertSame( $administrator_id, $result['user_id'] );
+		$this->assertEquals( $administrator_id, $result['user_id'] );
 		$this->assertContains( 'administrator', $result['roles'] );
 	}
 
@@ -37,7 +37,7 @@ class Tests_XMLRPC_wp_getProfile extends WP_XMLRPC_UnitTestCase {
 
 		$result = $this->myxmlrpcserver->wp_getProfile( array( 1, 'editor', 'editor', $fields ) );
 		$this->assertNotIXRError( $result );
-		$this->assertSame( $editor_id, $result['user_id'] );
+		$this->assertEquals( $editor_id, $result['user_id'] );
 
 		$expected_fields = array( 'user_id', 'email', 'bio' );
 		$keys            = array_keys( $result );
