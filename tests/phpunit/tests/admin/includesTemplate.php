4f7369963ebe3969b7ae9826f1fc1ec923ddb1d3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-31 07:27:32 +0300

    Update includesTemplate.php

--- tests/phpunit/tests/admin/includesTemplate.php
+++ tests/phpunit/tests/admin/includesTemplate.php
@@ -5,46 +5,46 @@
 class Tests_Admin_includesTemplate extends WP_UnitTestCase {
 
 	function test_equal() {
-		$this->assertSame( ' selected=\'selected\'', selected( 'foo', 'foo', false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( 'foo', 'foo', false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( 'foo', 'foo', false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( 'foo', 'foo', false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( '1', 1, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( '1', 1, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( '1', 1, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( '1', 1, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( '1', true, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( '1', true, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( '1', true, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( '1', true, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( 1, 1, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( 1, 1, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( 1, 1, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( 1, 1, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( 1, true, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( 1, true, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( 1, true, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( 1, true, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( true, true, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( true, true, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( true, true, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( true, true, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( '0', 0, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( '0', 0, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( '0', 0, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( '0', 0, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( 0, 0, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( 0, 0, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( 0, 0, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( 0, 0, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( '', false, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( '', false, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( '', false, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( '', false, false ) );
 
-		$this->assertSame( ' selected=\'selected\'', selected( false, false, false ) );
-		$this->assertSame( ' checked=\'checked\'', checked( false, false, false ) );
+		$this->assertEquals( ' selected=\'selected\'', selected( false, false, false ) );
+		$this->assertEquals( ' checked=\'checked\'', checked( false, false, false ) );
 	}
 
 	function test_notequal() {
-		$this->assertSame( '', selected( '0', '', false ) );
-		$this->assertSame( '', checked( '0', '', false ) );
+		$this->assertEquals( '', selected( '0', '', false ) );
+		$this->assertEquals( '', checked( '0', '', false ) );
 
-		$this->assertSame( '', selected( 0, '', false ) );
-		$this->assertSame( '', checked( 0, '', false ) );
+		$this->assertEquals( '', selected( 0, '', false ) );
+		$this->assertEquals( '', checked( 0, '', false ) );
 
-		$this->assertSame( '', selected( 0, false, false ) );
-		$this->assertSame( '', checked( 0, false, false ) );
+		$this->assertEquals( '', selected( 0, false, false ) );
+		$this->assertEquals( '', checked( 0, false, false ) );
 	}
 
 	/**
